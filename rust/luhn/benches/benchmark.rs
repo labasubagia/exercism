023053868546d1ac2bda54d43cdfbe8258852a5b
Laba Subagia <labasubagia22@gmail.com>
2023-06-23 23:07:47 +0800

    [luhn] update

--- rust/luhn/benches/benchmark.rs
+++ rust/luhn/benches/benchmark.rs
@@ -1,14 +1,9 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use luhn::{is_valid, is_valid2};
+use luhn::is_valid;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("is valid (check whitespace inside try_fold)", |b| {
-        b.iter(|| is_valid("234 567 891 234"))
-    });
-    c.bench_function("is valid (filter whitespace)", |b| {
-        b.iter(|| is_valid2("234 567 891 234"))
-    });
+    c.bench_function("is valid", |b| b.iter(|| is_valid("234 567 891 234")));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -12,16 +12,3 @@ pub fn is_valid(code: &str) -> bool {
         })
         .map_or(false, |(sum, count)| count >= 2 && sum % 2 == 0)
 }
-
-pub fn is_valid2(code: &str) -> bool {
-    code.chars()
-        .filter(|c| !c.is_whitespace())
-        .rev()
-        .try_fold((0, 0), |(sum, count), c| {
-            c.to_digit(10)
-                .map(|n| if count % 2 == 1 { n * 2 } else { n })
-                .map(|n| if n > 9 { n - 9 } else { n })
-                .map(|n| (sum + n, count + 1))
-        })
-        .map_or(false, |(sum, count)| count >= 2 && sum % 2 == 0)
-}
